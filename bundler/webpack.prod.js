a9f1a5d794f2994388b92a347abb8b4b24e0aaae
william-hackett <whackett03@gmail.com>
2024-11-01 00:44:29 -0400

    Refactor webpack.prod.js and add vercel.json
    This commit refactors the webpack.prod.js file to improve the production build configuration. It adds the necessary plugins and settings for generating optimized bundles and copying static assets. Additionally, it introduces a new vercel.json file to configure the deployment on Vercel.

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -1,15 +1,33 @@
+const path = require('path')
 const { merge } = require('webpack-merge')
 const commonConfiguration = require('./webpack.common.js')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = merge(
     commonConfiguration,
     {
         mode: 'production',
-        devtool: false,
+        output:
+        {
+            filename: 'bundle.[contenthash].js',
+            path: path.resolve(__dirname, '../dist')
+        },
         plugins:
         [
-            new CleanWebpackPlugin()
+            new CleanWebpackPlugin(),
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: 'static' }
+                ]
+            }),
+            new MiniCssExtractPlugin(),
+            new HtmlWebpackPlugin({
+                template: 'src/index.html',
+                minify: true
+            })
         ]
     }
 )
